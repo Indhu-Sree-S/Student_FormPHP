7c5c72c258838fc63e0efa90f65efe218f3205ac
Indhu Sree S <indhusrees16@gmail.com>
2021-11-18 13:21:45 +0530

    backend

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,7 +2,7 @@
 /* eslint-disable object-shorthand */
 /* eslint-disable no-var */
 /* eslint-disable @typescript-eslint/naming-convention */
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AlertController } from '@ionic/angular';
 
@@ -11,20 +11,43 @@ import { AlertController } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
   studentDetails = { StudentName: null, StudentRegNo: null, English: null, Tamil: null, Maths: null, Science: null, Social: null};
   baseURL = 'http://localhost/testDB/formdemo.php';
+  AllData: any = null;
+  choice: any = null;
+  showform = 0;
 
   constructor(
     private http: HttpClient,
     private alertCtrl: AlertController) {}
 
-  SubmitStudentDetails(submitform){
+  ngOnInit() {
+    this.ToGetAllData();
+  }
+
+  close(){
+    this.showform=0;
+  }
+
+  ToGetAllData(){
+    var message: any;
+    this.http.post(this.baseURL,{mode:'All'})
+    .subscribe(data1 => {
+     console.log(data1);
+     message = data1;
+     this.AllData = message.Data;}
+    );
+  }
+
+  SubmitStudentDetails(submitform,choice){
     var message: any;
     console.log(this.studentDetails);
     var sum = (this.studentDetails.English*1)+(this.studentDetails.Tamil*1)+(this.studentDetails.Maths*1)+(this.studentDetails.Science*1)+(this.studentDetails.Social*1);
     var avg = sum/5;
+    var mode = (choice==='Update')?'Update':'Add';
     const file =JSON.stringify({
+      mode: mode,
       StudentName: this.studentDetails.StudentName,
       StudentRegNo: this.studentDetails.StudentRegNo,
       English: this.studentDetails.English,
@@ -40,28 +63,42 @@ export class HomePage {
     .subscribe(data1 => {
      console.log(data1);
      message = data1;
-     this.successalert(message, submitform);
+     this.AllData = message.Data;
+     submitform.reset();
+     this.showform=0;
     },
     err => {
     console.log('ERROR!: ', err);
   });
   }
 
-  successalert(message, form){
-    this.alertCtrl.create({
-      header: 'Your Results',
-      message: 'Total : '+message.Total+' & Average : '+message.Average,
-      buttons: [
-        {
-        text: 'Okay',
-        handler: () => {
-          form.resetForm();
-        }
-      }]
-     }).then(alertEL => {
-       alertEL.present();
-     });
+  addStudent(){
+    this.showform=1;
   }
+
+  changeOnData(student,choice){
+    var message: any;
+    if(choice==='Update'){
+      this.http.post(this.baseURL,{id:student.StudentRegNo, mode:'SingleData'})
+      .subscribe(data1 => {
+        console.log(data1);
+        message = data1;
+        this.studentDetails = message.Data[0];
+        this.choice = choice;
+        this.showform=1;
+      });
+    }
+    else{
+      this.http.post(this.baseURL,{id:student.StudentRegNo, mode:'Delete'})
+      .subscribe(data1 => {
+        console.log(data1);
+        message = data1;
+        this.AllData = message.Data;
+      });
+    }
+
+  }
+
 
 
 }
