8316d17bd6c8fc24c7500114efe03c87d4a0efa4
Indhu Sree S <indhusrees16@gmail.com>
2021-11-23 09:40:59 +0530

    backend connection

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,6 +5,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AlertController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+
 
 @Component({
   selector: 'app-home',
@@ -17,6 +20,7 @@ export class HomePage implements OnInit{
   AllData: any = null;
   choice: any = null;
   showform = 0;
+  hint = null;
 
   constructor(
     private http: HttpClient,
@@ -28,21 +32,22 @@ export class HomePage implements OnInit{
 
   close(){
     this.showform=0;
+    this.studentDetails.StudentName= null; this.studentDetails.StudentRegNo= null;this.studentDetails.English= null; this.studentDetails.Tamil= null; this.studentDetails.Maths= null; this.studentDetails.Science= null; this.studentDetails.Social= null;
   }
 
   ToGetAllData(){
     var message: any;
-    this.http.post(this.baseURL,{mode:'All'})
+    //this.http.post(this.baseURL,{mode:'All'})
+    ajax({url:this.baseURL,method:'POST',body:JSON.stringify({mode: 'All'})})
     .subscribe(data1 => {
-     console.log(data1);
-     message = data1;
-     this.AllData = message.Data;}
+      console.log(data1.response);
+      message = data1;
+      this.AllData = message.response.Data;}
     );
   }
 
   SubmitStudentDetails(submitform,choice){
     var message: any;
-    console.log(this.studentDetails);
     var sum = (this.studentDetails.English*1)+(this.studentDetails.Tamil*1)+(this.studentDetails.Maths*1)+(this.studentDetails.Science*1)+(this.studentDetails.Social*1);
     var avg = sum/5;
     var mode = (choice==='Update')?'Update':'Add';
@@ -58,19 +63,40 @@ export class HomePage implements OnInit{
 	    Total: sum,
 	    Average: avg
     });
-    console.log(file);
-    this.http.post(this.baseURL,file)
+    //this.http.post(this.baseURL,file)
+    ajax({url:this.baseURL,method:'POST',body:file})
     .subscribe(data1 => {
-     console.log(data1);
      message = data1;
-     this.AllData = message.Data;
-     submitform.reset();
-     this.showform=0;
+     console.log(data1.response);
+     if(message.response.status){
+      this.AllData = message.response.Data;
+      submitform.reset();
+      this.showform=0;
+     }
     },
     err => {
     console.log('ERROR!: ', err);
   });
   }
+
+  /*showHint($event) {
+    console.log($event);
+    if ($event.length === 0) {
+        this.hint = '';
+        return;
+    } else {
+      var x: any;
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+              x = this.responseText;
+            }
+        };
+        x = this.hint;
+        xmlhttp.open('GET', 'http://localhost/get_data.php?q=' + $event, true);
+        xmlhttp.send();
+    }
+  }*/
 
   addStudent(){
     this.showform=1;
@@ -79,21 +105,33 @@ export class HomePage implements OnInit{
   changeOnData(student,choice){
     var message: any;
     if(choice==='Update'){
-      this.http.post(this.baseURL,{id:student.StudentRegNo, mode:'SingleData'})
+      //this.http.post(this.baseURL,{id:student.StudentRegNo, mode:'SingleData'})
+      ajax({url:this.baseURL,method:'POST',body:JSON.stringify({id:student.StudentRegNo, mode:'SingleData'})})
       .subscribe(data1 => {
-        console.log(data1);
         message = data1;
-        this.studentDetails = message.Data[0];
-        this.choice = choice;
-        this.showform=1;
+        console.log(data1.response);
+        if(message.response.status){
+          this.studentDetails = message.response.Data[0];
+          this.choice = choice;
+          this.showform=1;
+        }
+      },
+      err => {
+      console.log('ERROR!: ', err);
       });
     }
     else{
-      this.http.post(this.baseURL,{id:student.StudentRegNo, mode:'Delete'})
+      //this.http.post(this.baseURL,{id:student.StudentRegNo, mode:'Delete'})
+      ajax({url:this.baseURL,method:'POST',body:JSON.stringify({id:student.StudentRegNo, mode:'Delete'})})
       .subscribe(data1 => {
-        console.log(data1);
         message = data1;
-        this.AllData = message.Data;
+        console.log(data1.response);
+        if(message.response.status){
+          this.AllData = message.response.Data;
+        }
+      },
+      err => {
+      console.log('ERROR!: ', err);
       });
     }
 
@@ -102,3 +140,4 @@ export class HomePage implements OnInit{
 
 
 }
+
